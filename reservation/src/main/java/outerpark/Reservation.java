6cff0fa0aa7bb2933ef81e450c1037f92d276d48
zamvirus2 <84003381+zamvirus2@users.noreply.github.com>
2021-06-16 22:00:36 +0900

    Update Reservation.java
    Correlation : FeignClient를 통한 구현

--- reservation/src/main/java/outerpark/Reservation.java
+++ reservation/src/main/java/outerpark/Reservation.java
@@ -17,8 +17,19 @@ public class Reservation {
     private Long seats;
     private String status;
 
-    @PostPersist
-    public void onPostPersist(){
+
+    // *************************************************************************
+    // Correlation : FeignClient를 통한 구현
+    // *************************************************************************
+    // Reservation 수행시, Musical에 사전 등록된 좌석수(혹은 타 reservation을 통해 차감된 최종 남은 좌석수)를 초과하여 예약시도 할 경우,
+    // 신규 reservation의 persist(POST)가 되지 않도록 처리하여야 한다.
+    //
+    // 따라서 @PostPersist를 @PrePersist, onPrePersist()로 변경하고
+    // Musical MicroService에서 좌석수 체크 (http://localhost:8081/musicals의 /chkAndModifySeat) 결과가 false 로 나올 경우
+    // 강제 Exception을 발생시켜서, reservation 이 POST되지 않도록 처리한다.
+    // *************************************************************************
+    @PrePersist
+    public void onPrePersist() throws Exception{
 
         boolean rslt = ReservationApplication.applicationContext.getBean(outerpark.external.MusicalService.class)
             .modifySeat(this.getMusicalId(), this.getSeats().intValue());
@@ -28,6 +39,8 @@ public class Reservation {
             Reserved reserved = new Reserved();
             BeanUtils.copyProperties(this, reserved);
             reserved.publishAfterCommit();
+        }else{
+            throw new Exception("e");
         }
         //Following code causes dependency to external APIs
         // it is NOT A GOOD PRACTICE. instead, Event-Policy mapping is recommended.
